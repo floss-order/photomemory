ef429ad6aed7d97ef09fc759782c8e7bad4dd895
floss-order <flossOrder@gmail.com>
2020-06-17 09:24:07 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+
+//Server config
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.set('layout', 'layouts/layout.ejs')
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded( { limit: '10mb', extended: false } ))
+
+//Routes list
+
+
+//Server startup
+app.listen(process.env.PORT || 3000)
