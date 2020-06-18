0ecc3e057d0184740dad40e43b7d9382ae8449f5
floss-order <flossOrder@gmail.com>
2020-06-18 17:27:00 +0300

    Oh, something is missing!

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const bodyParser = require('body-parser')
 
 
 //Index page
 router.get('/', (req, res) => {
     const page = { title: 'PhotoMemory' }
     res.render('index.ejs', { page })
+})
+
+router.post('/', (req, res) => {
+    
 })
 
 
