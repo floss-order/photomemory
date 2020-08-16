abf7988a67a200ef9c185669897c66dc9779b88a
floss-order <flossOrder@gmail.com>
2020-08-16 16:12:25 +0300

    Implemented database connection

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const path = require('path')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 
 //Server config
@@ -19,5 +21,10 @@ const authRouter = require('./routes/auth.js')
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
 
-//Server startup
-app.listen(process.env.PORT || 3000)
+//Connect to the database and run the server
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
+    .then( () => {
+        console.log('Successfully connected to the database!')
+        app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`))
+    })
+    .catch((err) => { throw err } )
