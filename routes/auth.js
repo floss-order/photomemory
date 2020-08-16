7b511f8fe45329bd4316637acda22aad206decca
floss-order <flossOrder@gmail.com>
2020-08-16 11:22:06 +0300

    Sign up page has been refactored

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const router = express.Router()
 //Sign up route
 router.get('/sign-up', (req, res) => {
     const page = { title: 'Sign up', layout: false }
-    res.render('auth/register.ejs', { page })
+    res.render('auth/sign-up.ejs', { page })
 })
 
 //Sign in route
