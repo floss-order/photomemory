3fe76207b22653e75fa77f2871b1d65f50edbebc
floss-order <flossOrder@gmail.com>
2020-10-20 10:15:09 +0300

    Сhanged class names to match BEM

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -4,9 +4,9 @@ import './Searchbar.css'
 
 function Searchbar() {
     return (
-        <form className="search_form">
-          <label className="search_form__label" htmlFor="search-bar">Type the name of an Instagram account</label>
-          <input className="search_form__bar" type="search" placeholder="@username" name="search-bar" id="search-bar"/>
+        <form className="search-form">
+          <label className="search-form_label" htmlFor="search-bar">Type the name of an Instagram account</label>
+          <input className="search-form_bar" type="search" placeholder="@username" name="search-bar" id="search-bar"/>
           {/* <input type="submit"/> */}
         </form>
     )
