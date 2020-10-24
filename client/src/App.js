b28af56adc2535dd8f5a5235bb10ceb10de9b0ce
floss-order <flossOrder@gmail.com>
2020-10-24 14:18:43 +0300

    Set up routing for the home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
+
+import './App.css'
+import Container from './components/Container'
 import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+
+import { Switch, Route } from "react-router-dom"
 
 function App() {
   return (
-    <Header className="header"/>
+    <>
+      <Switch>
+        <Container>
+          <Header/>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Footer/>
+        </Container>
+      </Switch>
+    </>
   )
 }
 
