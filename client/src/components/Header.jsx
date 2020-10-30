d9183e128d5617311b2ee3569df5da275b77628b
floss-order <flossOrder@gmail.com>
2020-10-30 18:09:01 +0300

    Added links to the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import propTypes from 'prop-types'
 import classNames from 'classnames'
+import { Link } from 'react-router-dom'
 
 import './Header.css'
 import logo from '../logo.svg'
@@ -13,11 +14,16 @@ function Header({ className }) {
     )
     return (
         <header className={classes}>
-            <img src={logo} alt="Logo"/>
-
+            <Link to="/">
+                <img src={logo} alt="Logo"/>
+            </Link>
             <div>
-                <Button className="btn-sm btn-violet-inverted">sign in</Button>
-                <Button className="btn-sm btn-violet">sign up</Button>
+                <Link to="/sign-in">
+                    <Button className="btn-sm btn-violet-inverted">sign in</Button>
+                </Link>
+                <Link to="/sign-up">
+                    <Button className="btn-sm btn-violet">sign up</Button>
+                </Link>
             </div>
         </header>
     )
