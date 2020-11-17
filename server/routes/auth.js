dd8f9d3157b43c14ce6f355135c21682603394f5
floss-order <flossOrder@gmail.com>
2020-11-17 09:56:56 +0300

    Removed view rendering routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,12 +2,6 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 
-//Sign up route
-router.get('/sign-up', (req, res) => {
-    const page = { title: 'Sign up', layout: false }
-    res.render('auth/sign-up.ejs', { page })
-})
-
 //Sign up a new user
 router.post('/sign-up', async (req, res) => {
     const { fullName, phone, address, zip, email, password } = req.body
@@ -24,11 +18,6 @@ router.post('/sign-up', async (req, res) => {
 })
 
 //Sign in route
-router.get('/sign-in', (req, res) => {
-    const page = { title: 'Sign in' }
-    res.render('auth/sign-in.ejs', { page })
-})
-
 router.post('/sign-in', async (req, res) => {
     const { email, password } = req.body
 
@@ -49,7 +38,7 @@ router.post('/sign-in', async (req, res) => {
             }
         })
     }
- })
+})
 
 //Sign out the user
 router.post('/sign-out', (req, res) => {
