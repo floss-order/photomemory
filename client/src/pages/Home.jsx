982c94c02d28afea594096e4985896cf5ae8d0b4
floss-order <flossOrder@gmail.com>
2020-11-27 15:33:13 +0300

    User info fetching form validation using react-hook-form

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,30 +1,18 @@
 import React, { useState, useRef } from 'react'
+import { useForm } from 'react-hook-form'
+import api from '../utils/api'
 
 import './Home.css'
 import Label from '../components/Label'
 import Input from '../components/Input'
 import UsernameCard from '../components/UsernameCard'
+import Error from '../components/Error'
 
-async function getAccountInfo(account) {
-    let info = await fetch(`http://localhost:5000/${account}`)
-    info = await info.json()
-    return info
-}
 
 function Home() {
-    const handleSubmit = async e => {
-        e.preventDefault()
-
-        const info = await getAccountInfo(value)
-        setAccount(info)
-
-        setIsSubmit(true)
-        sectionRef.current.scrollIntoView({ behavior: 'smooth' })
-    }
-    
-    const [value, setValue] = useState('')
+    const {register, handleSubmit, setError, errors} = useForm()
     const [isSubmit, setIsSubmit] = useState(false)
-    const [account, setAccount] = useState({
+    const [userInfo, setUserInfo] = useState({
         username: '',
         profile_pic_url: '',
         posts: '',
@@ -33,6 +21,45 @@ function Home() {
     })
     const sectionRef = useRef()
     
+    async function onSubmit(data) {
+        
+        //Destructure the object and assign username propertry to a variable
+        const {username} = data 
+
+        try {
+            const response = await api.get(`/api/instagram/users/${username}`)
+            setUserInfo(response.data)
+
+            //Show the user info section and scroll down to it
+            setIsSubmit(true)
+            sectionRef.current.scrollIntoView({ behavior: 'smooth' })
+        }
+
+        catch(error) {
+
+            if(error.response) {
+                setError('username', {
+                    name: 'username',
+                    type: 'manual',
+                    message: error.response.data.error.description
+                })
+            } 
+
+            else if(error.request) {
+                setError('username', {
+                    name: 'username',
+                    type: 'manual',
+                    message: 'Network error. Check your internet connection or try later.'
+                })
+            }
+
+            else {
+                throw error
+            }
+
+        } 
+
+    }
 
     return (
         <>
@@ -41,19 +68,20 @@ function Home() {
                     Turn the brightest moments of your life from the Instagram 
                     into photobook with PhotoMemory!
                 </h1>
-                <form className="search-form" onSubmit={handleSubmit}>
+                <form className="search-form" onSubmit={handleSubmit(onSubmit)}>
                     <Label
                         className="label_search-form" 
-                        htmlFor="input_search-form" 
+                        htmlFor="username" 
                         text="Type the name of an Instagram account" 
                     />
+
+                    {errors.username && <Error text={errors.username.message} />}
                     <Input 
-                        id="input_search-form" 
+                        id="username" 
                         className="input_search-form" 
                         placeholder="username" 
                         type="text" 
-                        value={value} 
-                        onChange={e => {setValue(e.target.value)} } 
+                        ref={register} //добавить обработку ошибок и переписать на react-hook-form
                     />
                     <span className="search-form__input-badge">@</span>
                 </form>
@@ -63,21 +91,15 @@ function Home() {
                 <section className="account flex flex_justify-center flex_align-center" ref={sectionRef}>
                     <h1 className="section-heading">Account has been found!</h1>
                     <UsernameCard 
-                        username={account.username} 
-                        profile_pic_url={account.profile_pic_url} 
-                        posts={account.posts} 
-                        followers={account.followers}
-                        following={account.following}
+                        username={userInfo.username} 
+                        profile_pic_url={userInfo.profile_pic_url} 
+                        posts={userInfo.posts} 
+                        followers={userInfo.followers}
+                        following={userInfo.following}
                     />
                 </section>
             }
         </>
-    /*
-        Todo:
-        1. Получить из поисковой строки учётную запись
-        2. Передать в карточку в виде пропса
-        3. Валидировать пропс
-    */
     )
 }
 
