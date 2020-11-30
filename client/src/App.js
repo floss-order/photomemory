269bf7c16769931aa5d28c23af0780c2544a8b3b
floss-order <flossOrder@gmail.com>
2020-11-30 20:08:07 +0300

    Created routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './App.css'
 import Container from './components/Container'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import PhotoSelection from './pages/PhotoSelection'
 
 import { Switch, Route } from "react-router-dom"
+
 
 function App() {
   return (
     <>
-      <Switch>
-        <Container>
-          <Header/>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Footer/>
-        </Container>
-      </Switch>
+      <Container>
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/sign-in" component={SignIn} />
+          <Route path="/sign-up" component={SignUp} />
+          <Route path="/photo-selection" component={PhotoSelection} />
+        </Switch>
+        <Footer/>
+      </Container>
     </>
   )
 }
