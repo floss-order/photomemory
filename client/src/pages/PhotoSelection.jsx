d1e61a2a2c4cae055f29de1ed5301c531f8f726e
floss-order <flossOrder@gmail.com>
2021-02-25 15:58:26 +0000

    Created a responsive image grid

--- client/src/pages/PhotoSelection.jsx
+++ client/src/pages/PhotoSelection.jsx
@@ -1,83 +1,36 @@
 import React, { useState } from 'react'
+import Card from '../components/Card'
 
 import './PhotoSelection.css'
-import Card from '../components/Card'
-import Label from '../components/Label'
-import Dropdown from '../components/Dropdown'
 
+function PhotoSelection({ username }) {
+    const images = [
+        {
+            url: 'https://images.unsplash.com/photo-1614191697800-ff83bd175cee?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80'
+        },
 
-function sortByDate(date, dateOptions) {
-    switch(date.value) {
-        case dateOptions[0].value: {
-            
-        } 
-    }
-}
+        {
+            url: 'https://images.unsplash.com/photo-1614210482341-321710ec565e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1225&q=80'
+        },
 
+        {
+            url: 'https://source.unsplash.com/random'
+        },
 
-function PhotoSelection() {
-    const dateOptions = [
-        { value: "all", label: "All" },
-        { value: "this month", label: "This month" },
-        { value: "last month", label: "Last month" },
-        { value: "this year", label: "This year" },
-        { value: "last year", label: "Last year" }
+        {
+            url: 'https://source.unsplash.com/random'
+        }
     ]
-    const popularOptions = [
-        { value: 'default', label: 'Default'}, 
-        { value: 'likes', label: 'Likes'}, 
-        { value: 'comments', label: 'Comments'}
-    ]
-    const orderOptions = [
-        { value: 'ascending', label: 'Ascending'}, 
-        { value: 'descending', label: 'Descending'}
-    ]
-
-    const [dateValue, setDateValue] = useState(dateOptions[0])
-    const [popularValue, setPopularValue] = useState(popularOptions[0])
-    const [orderValue, setOrderValue] = useState(orderOptions[0])
-
-    function handleDateValue(date) {
-        setDateValue(date)
-    }
-
-    function handlePopularValue(popular) {
-        setPopularValue(popular)
-    }
-
-    function handleOrderValue(order) {
-        setOrderValue(order)
-    }
 
     return (
-        // <Card>
-            // <h1>Select photos</h1>
-
-            // <div className="gallery-container"> 
-                // <Label text="Filter"/>
-                
-                <div className="filter-container">
-                    <Dropdown 
-                    value={dateValue}
-                    options={dateOptions}
-                    onChange={handleDateValue}
-                    />
-
-                    <Dropdown 
-                    value={popularValue}
-                    options={popularOptions}
-                    onChange={handlePopularValue}
-                    />
-
-                    <Dropdown 
-                    value={orderValue}
-                    options={orderOptions}
-                    onChange={handleOrderValue}
-                    />
-                </div>
-
-            // </div>
-        // </Card>
+        <Card>
+            <h1 className="section-heading">Select photos</h1>
+            <div className="images">
+                {images.map(image => (
+                    <img className="images__item" src={image.url} />)
+                )}
+            </div>
+        </Card>
     )
 }
 
