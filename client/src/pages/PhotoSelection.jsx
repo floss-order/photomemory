ab5b6686b462ed71a6b5085d87dcfaede50b5434
floss-order <flossOrder@gmail.com>
2021-02-26 19:00:30 +0000

    The gallery is placed in a separate component

--- client/src/pages/PhotoSelection.jsx
+++ client/src/pages/PhotoSelection.jsx
@@ -1,36 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import Button from '../components/Button'
 import Card from '../components/Card'
+import SelectionGallery from '../components/SelectionGallery'
 
 import './PhotoSelection.css'
 
 function PhotoSelection({ username }) {
-    const images = [
-        {
-            url: 'https://images.unsplash.com/photo-1614191697800-ff83bd175cee?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80'
-        },
+    const [images, setImages] = useState([])
 
-        {
-            url: 'https://images.unsplash.com/photo-1614210482341-321710ec565e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1225&q=80'
-        },
-
-        {
-            url: 'https://source.unsplash.com/random'
-        },
-
-        {
-            url: 'https://source.unsplash.com/random'
-        }
-    ]
+    useEffect(() => {
+        const img = [
+            {
+                url: 'https://images.unsplash.com/photo-1614191697800-ff83bd175cee?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80'
+            },
+    
+            {
+                url: 'https://images.unsplash.com/photo-1614210482341-321710ec565e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1225&q=80'
+            },
+    
+            {
+                url: 'https://source.unsplash.com/random'
+            },
+    
+            {
+                url: 'https://source.unsplash.com/random'
+            }
+        ]
+        setImages(img)
+    }, [])
 
     return (
         <Card>
             <h1 className="section-heading">Select photos</h1>
-            <div className="images">
-                {images.map(image => (
-                    <img className="images__item" src={image.url} />)
-                )}
-            </div>
+            <SelectionGallery images={images} />
             <div className="button-container">
                 <Button className="btn-sm btn-violet-inverted">back</Button>
                 <Button className="btn-sm btn-violet">next</Button>
